c608ac3f59197e1bffba5cfd489dc434a7a03166
AkshayPatel-360 <axaymow@gmail.com>
2022-01-21 13:03:29 -0500

    Created demo blockchain
    Created blockchain which stores
    -index
    -timestamp
    -data
    -previous hash of the block
     run tested  and verified that it's working as intended

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
-  constructor(index, timestamp, data, previousHash = "") {
+  constructor(index, timestamp, data, previousHash = '') {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
-    this.hash = '';
+    this.hash = this.calculateHash();
   }
 
-  calculateHash(){
-      
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.timestamp +
+        this.previousHash +
+        JSON.stringify(this.data)
+    ).toString();
   }
 }
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2022", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let earthCoin = new Blockchain();
+earthCoin.addBlock(new Block(1, "20/10/2022", { amount: 4 }));
+earthCoin.addBlock(new Block(2, "22/10/2022", { amount: 44 }));
+
+console.log(JSON.stringify(earthCoin, null, 4));
