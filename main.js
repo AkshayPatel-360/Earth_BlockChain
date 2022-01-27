0738ee8d23554ca7e1911f80253396750bf80111
AkshayPatel-360 <axaymow@gmail.com>
2022-01-26 20:47:37 -0500

    Blockchain validation functionally implemented

--- main.js
+++ main.js
@@ -56,6 +56,12 @@ class Blockchain {
     return this.chain[this.chain.length - 1];
   }
 
+  addBlock(newBlock){
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
   minePendingTransactions(miningRewardAddress){
     let block = new Block(Date.now(),this.pendingTransactions);
     block.mineBlock(this.difficulty);
@@ -65,8 +71,25 @@ class Blockchain {
 
     this.pendingTransactions =[
       new Transaction(null,miningRewardAddress, this.miningReward)
-    ];
-    
+    ];  
+  }
+
+  isChainValid(){
+    for (let i=1; i<this.chain.length;i++){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i-1];
+
+      if(currentBlock.hash !== currentBlock.calculateHash()){
+        return false;
+      }
+
+      if(currentBlock.previousHash !== previousBlock.hash){
+        return false;
+      }
+      
+
+    }
+    return true;
   }
 }
 
